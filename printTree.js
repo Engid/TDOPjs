5d41cfd56eca6505e38d0b3d7dd952a938e69065
gid <gid@Gideo.domain>
2017-02-07 19:41:35 -0600

    renamed to printTree

---
+++ printTree.js
@@ -0,0 +1,12 @@
+let sourceFileName = process.argv[2];
+const parser = require("./parser.js");
+const fs = require("fs");
+const util = require("util");
+
+let source = fs.readFileSync(sourceFileName, 'utf8');
+let tree = parser(source);
+
+console.log("Printing Tree for " + sourceFileName + ":\n");
+console.log( util.inspect(tree, {depth:null}) );
+
+
